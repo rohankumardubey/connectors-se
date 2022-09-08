0cb51072964461944278d1b80056b7edc8afc89a
jenkins-git <jenkins-git@talend.com>
2022-09-08 12:23:02 +0000

    [jenkins-release] Release 1.37.0

--- common-stream-io/stream-rawtext/src/test/java/org/talend/components/common/stream/input/rawtext/RawTextRecordReaderTest.java
+++ common-stream-io/stream-rawtext/src/test/java/org/talend/components/common/stream/input/rawtext/RawTextRecordReaderTest.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright (C) 2006-2022 Talend Inc. - www.talend.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package org.talend.components.common.stream.input.rawtext;
 
 import org.junit.jupiter.api.Assertions;
@@ -22,7 +34,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class RawTextRecordReaderTest {
 
     @ParameterizedTest
-    @CsvSource({"ISO-8859-7,true", "ISO-8859-1,false"})
+    @CsvSource({ "ISO-8859-7,true", "ISO-8859-1,false" })
     public void rawTextTest(String encoding, boolean success) throws IOException {
         RawTextConfiguration rawTextConfiguration = new RawTextConfiguration();
         ExtendedRawTextConfiguration extendedRawTextConfiguration = new ExtendedRawTextConfiguration(encoding, false);
